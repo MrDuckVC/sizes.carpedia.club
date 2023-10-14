dfee2bd82bd995660250cf85f6653d6732ade06b
MrDuckVC <cunev2005@gmail.com>
2023-10-14 20:33:21 +0300

    CSRF fix.

--- backend/carpedia/settings.py
+++ backend/carpedia/settings.py
@@ -136,3 +136,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    # CSRF settings
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
